c6956f661e53dc1d92050db460559a0b6af379cf
tomtrasmontero <tomtrasmontero@gmail.com>
2018-02-07 23:28:08 -0500

    added styling and semantic ui css framework

---
+++ client/src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import classes from './Footer.scss';
+
+const footer = () => (
+  <footer>
+    <div className={classes.Footer} >
+      <p>Made by TT 2017</p>
+    </div>
+  </footer>
+);
+
+export default footer;
--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import Aux from './hoc/Aux/Aux';
+import Footer from './components/Footer/Footer';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const app = (
+  <Aux>
+    <App />
+    <Footer />
+  </Aux>
+);
+
+ReactDOM.render(app, document.getElementById('root'));
 registerServiceWorker();
