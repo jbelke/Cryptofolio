bead816a9850f5905c4dc2881756206166ab9f50
tomtrasmontero <tomtrasmontero@gmail.com>
2018-02-12 01:46:56 -0500

    added signup form and begin setting up auth

--- client/src/containers/SignUp/SignUp.js
+++ client/src/containers/SignUp/SignUp.js
@@ -1,18 +1,68 @@
 import React, { Component } from 'react';
+import { Field, reduxForm, propTypes } from 'redux-form';
+import { connect } from 'react-redux';
+// import PropTypes from 'prop-types';
 
 class SignUp extends Component {
-  state = {
-    userData: 'user data',
+  handleSubmit = (values) => {
+    console.log(values, this.props);
+    // reset field
+    this.props.reset();
   }
+  // redux-form === prevent default hoc
+  renderField = ({
+    input, label, type, meta: { touched, error, warning },
+  }) => (
+    <div>
+      <input {...input} placeholder={label} type={type} />
+      {
+        touched && (
+          (error && <span>{error}</span>) || (warning && <span>{warning}</span>)
+        )
+      }
+      {this.props.errorMessage}
+    </div>
+  );
 
   render() {
+    const { handleSubmit } = this.props;
     return (
-      <div>
-        <p>signup</p>
-        <p>login : {this.state.userData}</p>
-      </div>
+      <form onSubmit={handleSubmit(this.handleSubmit)}>
+        <div>
+          <p>UserName</p>
+          <Field name="inputName" component={this.renderField} type="text" label="Name" />
+        </div>
+        <div>
+          <Field name="password" component={this.renderField} type="password" label="Password" />
+        </div>
+        <p>error :</p>
+        <button type="submit" onClick={() => console.log(this.props)}>Submit</button>
+      </form>
     );
   }
 }
 
-export default SignUp;
+// PropTypes here
+SignUp.propTypes = {
+  ...propTypes,
+};
+
+// validation func here
+const validate = (values) => {
+  const errors = {};
+  if (!values.inputName) {
+    errors.inputName = 'User Name is Required';
+  }
+
+  if (!values.password) {
+    errors.password = 'Please Enter Comment';
+  }
+
+  return errors;
+};
+
+export default reduxForm({
+  validate,
+  // name the form component
+  form: 'SignUpForm',
+})(connect(null, null)(SignUp));
--- client/src/store/reducers/index.js
+++ client/src/store/reducers/index.js
@@ -1,10 +1,13 @@
 import { combineReducers } from 'redux';
+import { reducer as formReducer } from 'redux-form';
 import CoinReducer from './coins';
 import NewsReducer from './news';
+
 
 const rootReducer = combineReducers({
   coin: CoinReducer,
   news: NewsReducer,
+  form: formReducer,
 });
 
 export default rootReducer;
